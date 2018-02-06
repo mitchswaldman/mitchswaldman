54e98b376e3238c457f50ee956edb35ab1ace05b
Mitchell Waldman <mitchswaldman@gmail.com>
2018-02-05 23:06:33 -0800

    Try something a little new by adding in tile cards.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import './App.css';
 
 class App extends Component {
   render() {
+    
+    const message = ["Hello ", "My", "name", "is", "Mitch"]
+    const colorClasses = ['clr1', 'clr2', 'clr3', 'clr4', 'clr5']
+
+    const renderWordWithColorClass = (word, color) => {
+      return word.split("").map( (letter, idx) => {
+        return (
+          <div className={`letter-box ${color}`} key={`${letter}-${idx}-${color}`}>
+            <p>{letter}</p>
+          </div>
+        )
+      })
+    }
+
     return (
       <div className="wrapper">
-        <div className='title'>
-          <h1 className='title__main'>Mitch Waldman</h1>
-          <h4 className='title__sub'>Developer. Musician. Dog lover.</h4>
-        </div>
-        <ul>
-          <li><a href='#'><i class="fas fa-code fa-2x"></i></a></li>
-          <li><a href='#'><i class="fas fa-headphones fa-2x"></i></a></li>
-          <li><a href='#'><i class="fas fa-paw fa-2x"></i></a></li>
-        </ul>
-        <div className='description'>
-          <p>My name is Mitch Waldman. This is my website, made better by the fact that you're here. Thanks. </p>
-          <p>I'm a developer from San Jose CA, a bedroom musician, and a dog lover. </p>
-        </div>
+        {message.map((word, index)=> {
+          return renderWordWithColorClass(word, colorClasses[index])
+        })}
       </div>
     );
   }
