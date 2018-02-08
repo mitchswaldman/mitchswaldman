2980d2241ca7b1d36bed3c6438a91f961fa8a01d
Mitchell Waldman <mitchswaldman@gmail.com>
2018-02-07 20:15:39 -0800

    Adjust visual to do frequency analysis. adjust package.json to push to this repo.

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -25,60 +25,60 @@ class Visualizer extends React.Component {
 		let dataArray = new Uint8Array(bufferLength)
 		ctx.clearRect(0, 0, WIDTH, HEIGHT)
 
-		
+
 		const draw = (t) => {
 			drawVisual = window.requestAnimationFrame(draw)
 			ctx.fillStyle = 'rgb(255, 255, 255)';
 			ctx.fillRect(0, 0, WIDTH, HEIGHT);
 			analyserNode.getByteFrequencyData(dataArray)
 			ctx.strokeStyle = strokeColor
+			ctx.fillStyle = strokeColor
 			ctx.beginPath()
 			let sliceWidth = WIDTH * 1.0 / bufferLength
 			let x = 0
 
+			ctx.beginPath()
+			ctx.moveTo(0, HEIGHT)
 			for (let i = 0; i < bufferLength; i++) {
 				var v = dataArray[i]
 				var y = HEIGHT  - SCALE * v
 
-				if ( i === 0) {
-					ctx.moveTo(x, y)
-				} else {
-					ctx.lineTo(x, y)
-				}
+				ctx.lineTo(x, y)
 
 				x += sliceWidth
 			}
 
-			ctx.lineTo(WIDTH, HEIGHT/2)
-			ctx.stroke()
+			ctx.lineTo(WIDTH, HEIGHT)
+			ctx.lineTo(0, HEIGHT)
+			ctx.fill()
 		}
 
 		draw()
 	}
 
-	// componentWillReceiveProps({lastSample = 'Hello'}) {
-	// 	const firstTwo = lastSample.substring(0, 2).toLowerCase()
-	// 	console.log(firstTwo)
-	// 	switch (firstTwo) {
-	// 		case 'he':
-	// 			strokeColor = '#f6d365'
-	// 			return
-	// 		case 'my':
-	// 			strokeColor = '#fbc2eb'
-	// 			return 
-	// 		case 'na':
-	// 			strokeColor = '#84fab0'
-	// 			return
-	// 		case 'is':
-	// 			strokeColor = '#a1c4fd'
-	// 			return 
-	// 		case 'mi':
-	// 			strokeColor = '#ffecd2'
-	// 			return
-	// 		default:
-	// 			strokeColor ='#000'
-	// 	}
-	// }
+	componentWillReceiveProps({lastSample = 'Hello'}) {
+		const firstTwo = lastSample.substring(0, 2).toLowerCase()
+		console.log(firstTwo)
+		switch (firstTwo) {
+			case 'he':
+				strokeColor = '#f6d365'
+				return
+			case 'my':
+				strokeColor = '#fbc2eb'
+				return 
+			case 'na':
+				strokeColor = '#84fab0'
+				return
+			case 'is':
+				strokeColor = '#a1c4fd'
+				return 
+			case 'mi':
+				strokeColor = '#ffecd2'
+				return
+			default:
+				strokeColor ='#000'
+		}
+	}
 
 	componentWillUnmount() {
 		window.cancelAnimationFrame(drawVisual)
